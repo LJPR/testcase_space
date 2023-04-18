103220dcdc831ce7f776ab69ac3c6603e790a0d9
LJPR <LJPR@Protonmail.com>
2023-04-18 00:10:16 -0400

    to correct branch this time

--- src/pages/zoombies-setup.tsx
+++ src/pages/zoombies-setup.tsx
@@ -1,6 +1,4 @@
-
-
-
+/*
 <input type="range" min="0" max="100" value="25" className="range" step="25" />
 <div className="w-full flex justify-between text-xs px-2">
   <span>|</span>
@@ -9,3 +7,4 @@
   <span>|</span>
   <span>|</span>
 </div>
+ */
